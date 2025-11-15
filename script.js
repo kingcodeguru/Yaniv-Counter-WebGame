7be6ff821adeadb26e4fe34c274cb965bcda1859
Liel Avraham <liel.av07@email.com>
2025-11-15 18:30:08 +0200

    copying from tic-tac-toe, now finished updated html

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const gameBoard = document.getElementById('player-list');
+    const cells = Array.from(document.querySelectorAll('.cell'));
+    const restartButton = document.getElementById('restart-button');
+    const gameStatus = document.getElementById('game-status');
+
+    let board = ['', '', '', '', '', '', '', '', '']; // Represents the 3x3 board
+    let currentPlayer = 'X';
+    let isGameActive = true;
+});
+const playerName = document.getElementById('getName');
+document.getElementById('addPlayer').addEventListener('click', () => {
+    if (!playerName) {
+        return;
+    }
+
+});
